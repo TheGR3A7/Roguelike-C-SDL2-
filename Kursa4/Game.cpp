78d13c88dd60b92ceaff3f7efa20bce14cce1401
TheGR3A7 <AlexeyLogvinov97@gmail.com>
2023-11-01 14:30:45 +0300

    full map

--- Kursa4/Game.cpp
+++ Kursa4/Game.cpp
@@ -7,16 +7,17 @@
 #include "func.h"
 #include "Player.h"
 
-#define MAP_WIDTH 10
-#define MAP_HEIGHT 8
-#define TILE_SIZE 25
+#define MAP_WIDTH 20
+#define MAP_HEIGHT 12
+#define TILE_SIZE 50
 
-char mapData[MAP_HEIGHT][MAP_WIDTH];
+char surfData[MAP_HEIGHT][MAP_WIDTH];
+char obstData[MAP_HEIGHT][MAP_WIDTH];
 
-void LoadMapDataFromFile(const char* filename)
+void LoadMapDataFromFile(const char* surfaceFilename, const char* obstacleFilename)
 {
-	FILE* file = fopen(filename, "r");
-	if (file == NULL)
+	FILE* surfaceFile = fopen(surfaceFilename, "r");
+	if (surfaceFile == NULL)
 	{
 		printf("Не удалось открыть файл с данными карты.\n");
 		exit(1);
@@ -28,77 +29,160 @@ void LoadMapDataFromFile(const char* filename)
 		{
 			char symbol;
 
-			while (fscanf(file, "%c", &symbol) == 1)
+			while (fscanf(surfaceFile, "%c", &symbol) == 1)
 			{
 				if (symbol != ' ' && symbol != '\n')
 				{
-					mapData[i][j] = symbol;
+					surfData[i][j] = symbol;
 					break;
 				}
 			}
 		}
 	}
 
-	fclose(file);
+	fclose(surfaceFile);
+
+	FILE* obstacleFile = fopen(obstacleFilename, "r");
+	if (obstacleFile == NULL)
+	{
+		printf("Не удалось открыть файл с данными карты.\n");
+		exit(1);
+	}
+
+	for (int i = 0; i < MAP_HEIGHT; i++)
+	{
+		for (int j = 0; j < MAP_WIDTH; j++)
+		{
+			char symbol;
+
+			while (fscanf(obstacleFile, "%c", &symbol) == 1)
+			{
+				if ( symbol != '\n')
+				{
+					obstData[i][j] = symbol;
+					break;
+				}
+			}
+		}
+	}
+
+	fclose(obstacleFile);
 }
 
 
 
-void LoadMapElements(SDL_Renderer* ren, MapElement mapElements[MAP_WIDTH][MAP_HEIGHT])
+void LoadSurfaceElements(SDL_Renderer* ren, MapElement surfElements[MAP_HEIGHT][MAP_WIDTH])
 {
-	for (int i = 0; i < MAP_WIDTH; i++) 
+	for (int i = 0; i < MAP_HEIGHT; i++) 
 	{
-		for (int j = 0; j < MAP_HEIGHT; j++) 
+		for (int j = 0; j < MAP_WIDTH; j++) 
 		{
-			mapElements[i][j].position.x = i * TILE_SIZE;
-			mapElements[i][j].position.y = j * TILE_SIZE;
-			mapElements[i][j].position.w = TILE_SIZE;
-			mapElements[i][j].position.h = TILE_SIZE;
-			char symbol = mapData[i][j]; // Инвертируйте индексы, если необходимо
-			if (symbol == '#') 
-			{
-				mapElements[i][j].texture = loadTextureFromFile("wall.png", &(mapElements[i][j].position), ren); 
-			}
-			else if (symbol == '/') {
-				mapElements[i][j].texture = loadTextureFromFile("door.png", &(mapElements[i][j].position), ren);
-			}
-			else if (symbol == '.') {
-				mapElements[i][j].texture = loadTextureFromFile("asphalt.jpg", &(mapElements[i][j].position), ren);
-			}
-			else if (symbol == '=') {
-				mapElements[i][j].texture = loadTextureFromFile("fence_h.png", &(mapElements[i][j].position), ren);
-			}
-			else if (symbol == '|') {
-				mapElements[i][j].texture = loadTextureFromFile("fence_v.png", &(mapElements[i][j].position), ren);
+			char symbol = surfData[i][j]; 
+			if (symbol == '.') {
+				surfElements[i][j].texture = loadTextureFromFile("asphalt.jpg", &(surfElements[i][j].position), ren);
 			}
 			else if (symbol == ',') {
-				mapElements[i][j].texture = loadTextureFromFile("light_tile.jpg", &(mapElements[i][j].position), ren);
+				surfElements[i][j].texture = loadTextureFromFile("light_tile.jpg", &(surfElements[i][j].position), ren);
 			}
 			else if (symbol == '"') {
-				mapElements[i][j].texture = loadTextureFromFile("sidewalk.png", &(mapElements[i][j].position), ren);
+				surfElements[i][j].texture = loadTextureFromFile("sidewalk.png", &(surfElements[i][j].position), ren);
 			}
-			else if (symbol == '*') {
-				mapElements[i][j].texture = loadTextureFromFile("roof.png", &(mapElements[i][j].position), ren);
-			}
-			mapElements[i][j].symbol = symbol;
+			surfElements[i][j].position.x = j * TILE_SIZE;
+			surfElements[i][j].position.y = i * TILE_SIZE;
+			surfElements[i][j].position.w = TILE_SIZE;
+			surfElements[i][j].position.h = TILE_SIZE;
+			surfElements[i][j].symbol = symbol;
 		}
 	}
 }
 
+void LoadObstacleElements(SDL_Renderer* ren, MapElement obstElements[MAP_HEIGHT][MAP_WIDTH])
+{
+	for (int i = 0; i < MAP_HEIGHT; i++)
+	{
+		for (int j = 0; j < MAP_WIDTH; j++)
+		{
+			char symbol = obstData[i][j];
+			if (symbol == '#')
+			{
+				obstElements[i][j].texture = loadTextureFromFile("wall.png", &(obstElements[i][j].position), ren);
+			}
+			else if (symbol == '/') {
+				obstElements[i][j].texture = loadTextureFromFile("door.png", &(obstElements[i][j].position), ren);
+			}
+			else if (symbol == '=') {
+				obstElements[i][j].texture = loadTextureFromFile("fence_h.png", &(obstElements[i][j].position), ren);
+			}
+			else if (symbol == '|') {
+				obstElements[i][j].texture = loadTextureFromFile("fence_v.png", &(obstElements[i][j].position), ren);
+			}
+			else if (symbol == '*') {
+				obstElements[i][j].texture = loadTextureFromFile("roof.png", &(obstElements[i][j].position), ren);
+			}
+			else if (symbol == ' ') {
+				obstElements[i][j].texture = nullptr;
+			}
+			obstElements[i][j].position.x = j * TILE_SIZE;
+			obstElements[i][j].position.y = i * TILE_SIZE;
+			obstElements[i][j].position.w = TILE_SIZE;
+			obstElements[i][j].position.h = TILE_SIZE;
+			obstElements[i][j].symbol = symbol;
+		}
+	}
+}
 
+// Loc 1: y (200, 400), x>1000
+// Loc 2: y (200, 400), x<1000 || y (50, 550), x > 1000
+// Loc 3: y (50, 550), x < 1000
+
+void CheckLocation(Player player, bool location1, bool location2, bool location3)
+{
+	if (location1 = true && (player.position.y >= 200 && player.position.x >= 1000 && player.position.y <= 400))
+	{
+		location1 = false;
+		LoadMapDataFromFile("surfLoc2.txt", "obstLoc2.txt");
+		location2 = true;
+	}
+
+	if (location2 = true && (player.position.y >= 200 && player.position.x <= 0 && player.position.y <= 400))
+	{
+		location2 = false;
+		LoadMapDataFromFile("surfLoc1.txt", "obstLoc1.txt");
+		location1 = true;
+	}
+
+	if (location2 = true && (player.position.y >= 50 && player.position.x >= 1000 && player.position.y <= 550))
+	{
+		location2 = false;
+		LoadMapDataFromFile("surfLoc3.txt", "obstLoc3.txt");
+		location3 = true;
+	}
+
+	if (location3 = true && (player.position.y >= 50 && player.position.x <= 0 && player.position.y <= 550))
+	{
+		location3 = false;
+		LoadMapDataFromFile("surfLoc2.txt", "obstLoc2.txt");
+		location2 = true;
+	}
+}
 
 void Game(SDL_Renderer* ren)
 {
 	bool isRunning = true;
+	bool location1 = true;
+	bool location2 = false;
+	bool location3 = false;
 
 	// Загрузка данных карты из файла
-	LoadMapDataFromFile("map.txt");
+	LoadMapDataFromFile("surfLoc1.txt", "obstLoc1.txt");
 
 	// Создание массива для элементов карты
-	MapElement mapElements[MAP_WIDTH][MAP_HEIGHT];
+	MapElement surfElements[MAP_HEIGHT][MAP_WIDTH]; 
+	MapElement obstElements[MAP_HEIGHT][MAP_WIDTH];
 
 	// Загрузка текстур элементов карты
-	LoadMapElements(ren, mapElements);
+	LoadSurfaceElements(ren, surfElements);
+	LoadObstacleElements(ren, obstElements);
 
     Player player;
     InitializePlayer(&player, ren, "HeisDef.png", 100, 100);
@@ -147,17 +231,21 @@ void Game(SDL_Renderer* ren)
 					break;
 				case SDL_SCANCODE_W:
 					isUpPressed = true;
+					CheckLocation(player, location1, location2, location3);
 					break;
 				case SDL_SCANCODE_S:
 					isDownPressed = true;
+					CheckLocation(player, location1, location2, location3);
 					break;
 				case SDL_SCANCODE_D:
 					isRightPressed = true;
 					player.direction = DIR_RIGHT;
+					CheckLocation(player, location1, location2, location3);
 					break;
 				case SDL_SCANCODE_A:
 					isLeftPressed = true;
 					player.direction = DIR_LEFT;
+					CheckLocation(player, location1, location2, location3);
 					break;
 				}
 				break;
@@ -191,15 +279,17 @@ void Game(SDL_Renderer* ren)
         SDL_RenderClear(ren);
 
 		// Отрисовка элементов карты
-		for (int i = 0; i < MAP_WIDTH; i++)
+		for (int i = 0; i < MAP_HEIGHT; i++)
 		{
-			for (int j = 0; j < MAP_HEIGHT; j++)
+			for (int j = 0; j < MAP_WIDTH; j++)
 			{
-				SDL_RenderCopy(ren, mapElements[i][j].texture, NULL, &(mapElements[i][j].position));
+				SDL_RenderCopy(ren, surfElements[i][j].texture, NULL, &(surfElements[i][j].position));
+				if(obstElements[i][j].texture != nullptr)
+					SDL_RenderCopy(ren, obstElements[i][j].texture, NULL, &(obstElements[i][j].position));
 			}
 		}
 
-        // Отрисовка игрока
+        // Отрисовка игрока, врагов
 		RenderPlayer(&player, ren);
 
 		RenderEnemy(&hector, ren);
